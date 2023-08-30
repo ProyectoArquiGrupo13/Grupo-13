9ce91280c0cb14798d7b7ab7a8e7a3fbb9822fd1
ProyectoArquiGrupo13 <141192261+ProyectoArquiGrupo13@users.noreply.github.com>
2023-08-30 11:18:09 -0500

    Update firebase.js

--- js/grupo13/firebase.js
+++ js/grupo13/firebase.js
@@ -4,19 +4,20 @@
 
 $(document).ready(function () {
 	const config = {
-		apiKey: "AIzaSyB4XD-AIzaSyDUAEKVhvQV4g9xk5uefVGAPpRJwPFZTDs",
-		authDomain: "presion-a3041.firebaseapp.com",
-		databaseURL: "https://presion-a3041-default-rtdb.firebaseio.com/",
-		projectId: "presion-a3041",
-		storageBucket: "presion-a3041.appspot.com",
-		messagingSenderId: "752009347781",
-		appId: "1:752009347781:web:7dc65ecf2f5bc204fe26ac",
+		apiKey: "AIzaSyCHhmPm3sPNgDco-ley8-pvS8-qOjwO30I",
+		authDomain: "arquitectura-grupo-13.firebaseapp.com",
+		databaseURL: "https://arquitectura-grupo-13-default-rtdb.firebaseio.com",
+		projectId: "arquitectura-grupo-13",
+		storageBucket: "arquitectura-grupo-13.appspot.com",
+		messagingSenderId: "114150102470",
+		appId: "1:114150102470:web:3fe36e460c771fc0e88ec7",
+		measurementId: "G-VMSQWRJBTX"
 	};
 	firebase.initializeApp(config); //inicializamos firebase
 	var db = firebase.database();
-	var coleccionProductos = db.ref().child("presiones");
+	var coleccionProductos = db.ref().child("valores");
 	var dataSet = []; //array para guardar los valores de los campos inputs del form
-	var table = $("#tablaProductos").DataTable({
+	var table = $("#tablaValores").DataTable({
 		pageLength: 5,
 		lengthMenu: [
 			[5, 10, 20, -1],
@@ -32,14 +33,17 @@ $(document).ready(function () {
 	});
 
 	coleccionProductos.on("child_added", (datos) => {
-		$("#tablaProductos").delay("slow").fadeIn();
+		$("#tablaValores").delay("slow").fadeIn();
 		dataSet = [
 			datos.key,
 			datos.child("fecha").val(),
 			datos.child("hora").val(),
-			datos.child("presionSistolica").val(),
-			datos.child("presionDiastocia").val(),
+			datos.child("CPU").val(),
+			datos.child("Memoria").val(),
+			datos.child("Disco").val(),
+			datos.child("Recepcion").val(),
 		];
 		table.rows.add([dataSet]).draw();
 	});
 });
+
